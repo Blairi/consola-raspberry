98c4cd9321570a191b36fb3d0d7084b3bbfa44f5
Tomás Nieto <159396548+tomasnieto02@users.noreply.github.com>
2025-11-25 19:59:14 -0600

    Update x_snes.sh

--- src/x_snes.sh
+++ src/x_snes.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-# Script para lanzar Snes9x en tty3 de forma limpia
+# MIT License
+
+# Copyright (c) 2025 Nieto Rodríguez Tomás Andrés
+# Copyright (c) 2025 Guadarrama Herrera Ken Bryan
+# Copyright (c) 2025 Montiel Aviles Axel Fernando
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 killall -9 python3
 python3 GestorUSB.py &
-# Cambiar a tty3
 sudo chvt 3
-
-# Lanzar Xorg en tty3 con configuración mínima
 startx
 killall -9 snes9x
 chvt 1
